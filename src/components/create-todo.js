d50afbea5ed2442195e4beab1f15d25d5f2848f0
andrewgurung <andrewgurungg@gmail.com>
2017-04-27 21:05:28 -0400

    Create task handler

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -3,10 +3,15 @@ import React from 'react';
 export default class CreateTodo extends React.Component {
   render() {
     return(
-      <form>
-        <input type="text" placeholder="What do I need to do?" />
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="What do I need to do?" ref="createInput"/>
         <button>Create</button>
       </form>
     );
+  }
+
+  handleCreate(event) {
+    event.preventDefault();
+    this.props.createTask(this.refs.createInput.value);
   }
 }
