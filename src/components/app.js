690c97334c1c122e443b5eb3abfdf5464848a93c
andrewgurung <andrewgurungg@gmail.com>
2017-04-27 20:53:00 -0400

    Add save, edit,cancel handler

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TodosList from './todos-list';
+import CreateTodo from './create-todo';
+
 
 const todos = [
   {
@@ -13,11 +15,20 @@ const todos = [
 ];
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    }
+  }
+
   render() {
     return(
       <div>
           <h1>React ToDos App</h1>
-          <TodosList />
+          <CreateTodo />
+          <TodosList todos={this.state.todos}/>
       </div>
     );
   }
